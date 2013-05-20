6001f90a84261db879cd2e8567d86511a1fe4cb3
dharmeshkakadia <dharmesh.kakadia@gmail.com>
2013-05-20 22:06:14 +0530

    complete map-reduce and dfs-only

--- hadoop_install.sh
+++ hadoop_install.sh
@@ -50,8 +50,9 @@ HDFS_URI=hdfs://`grep -i namenode $PROPERTIES_FILE | cut -f 2`
 JOBTRACKER=`grep -i jobtracker $PROPERTIES_FILE  | cut -f 2`
 DFS_REPLICATION=2
 NN_FORMAT=1
-MASTER=`grep -i jobtracker $PROPERTIES_FILE  | cut -f 2 | cut -d ":" -f 1`
-SLAVES=`grep -i tasktracker $PROPERTIES_FILE  | cut -f 2`
+NAMENODE=`grep -i namenode $PROPERTIES_FILE  | cut -f 2 | cut -d ":" -f 1`
+DATANODE=`grep -i datanode $PROPERTIES_FILE  | cut -f 2`
+TASKTRACKER=`grep -i tasktracker $PROPERTIES_FILE  | cut -f 2`
 
 MAP_REDUCE_ONLY=0
 DFS_ONLY=0
@@ -70,8 +71,8 @@ while getopts "d:j:p:vhmf" opt; do
    o ) 	NN_FORMAT=0;;
    v )	VERBOSE=1;;
    m )  MAP_REDUCE_ONLY=1;;
-   f )  DFS_ONLY=1
-        NN_FORMAT=0;;
+   f )  DFS_ONLY=1;;
+#        NN_FORMAT=0;;
    h )  usage ;;
    \?)  echo "Invalid Option: $OPTARG"
    		usage
@@ -99,7 +100,7 @@ hadoop_tar=`find -name hadoop*.tar.gz`
 
 if [ ! -f "$hadoop_tar" ];
 then
-	echo "Hadoop Tar not found at location : $hadoop_tar. You can download form hadoop mirror using -d option"
+	echo "Hadoop tar not found at location : $hadoop_tar. You can download form hadoop mirror using -d option"
 	exit
 fi
 
@@ -113,21 +114,19 @@ function configure(){
 	cd $HADOOP_DIR/conf/
 	# actually not needed, used by start-all.sh and stop-all.sh
 	# master and slave files
-	echo $MASTER > masters
-	echo $SLAVES > slaves
+	echo $NAMENODE > masters
+	echo $DATANODE > slaves
 
 	echo "export JAVA_HOME=$JAVA_HOME" >> hadoop-env.sh
 
-    if [ $MAP_REDUCE_ONLY -ne 1 ] ;then
-	#core-site.xml
+    if [ $MAP_REDUCE_ONLY -ne 1 ] ;then #core-site.xml
 	echo -e "<?xml version=\"1.0\"?><?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?><configuration><property><name>hadoop.tmp.dir</name><value>$HADOOP_TEMP</value><description>A base for other temporary directories.</description></property><property><name>fs.default.name</name><value>$HDFS_URI</value><description>The name of the default file system.  A URI whosescheme and authority determine the FileSystem implementation.  Theuri's scheme determines the config property (fs.SCHEME.impl) namingthe FileSystem implementation class.  The uri's authority is used todetermine the host, port, etc. for a filesystem.</description></property></configuration>" > core-site.xml
 
     # hdfs-site.xml
 	echo -e "<?xml version=\"1.0\"?><?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?><configuration><property><name>dfs.replication</name><value>$DFS_REPLICATION</value><description>Default block replication.The actual number of replications can be specified when the file is created.The default is used if replication is not specified in create time.</description></property></configuration>" > hdfs-site.xml
     fi
 
-    if [ $DFS_ONLY -ne 1 ] ; then
-	#mapred-site.xml
+    if [ $DFS_ONLY -ne 1 ] ; then	#mapred-site.xml
 	echo -e "<?xml version=\"1.0\"?><?xml-stylesheet type=\"text/xsl\" href=\"configuration.xsl\"?><configuration><property><name>mapred.job.tracker</name><value>$JOBTRACKER</value><description>The host and port that the MapReduce job tracker runs </description></property></configuration>" > mapred-site.xml
 
     fi
@@ -139,8 +138,8 @@ function copyToSlaves(){
 	for srv in $1 ; do
 	  echo "Copying to $srv...";
 	  rsync -az --exclude='logs/*' $HADOOP_DIR/ $srv:$HADOOP_DIR/
-#	  ssh $srv "rm -fR /app"
-	done ]
+	  ssh $srv "rm -fR /app"
+	done
 }
 
 # Verify the status of hadoop-daemons
@@ -159,7 +158,11 @@ tar xzf $hadoop_tar -C $INSTALL_DIR
 sudo mv $INSTALL_DIR/hadoop-* $HADOOP_DIR
 
 configure $HADOOP_TEMP $2 $JAVA_HOME
-copyToSlaves $SLAVES
+
+copyToSlaves $DATANODE
+copyToSlaves $NAMENODE
+copyToSlaves $TASKTRACKER
+copyToSlaves $JOBTRACKER
 
 echo "-----------starting hadoop cluster-------------"
 # format namenode
@@ -167,17 +170,30 @@ cd $HADOOP_DIR/bin/
 
 if [ $NN_FORMAT -eq 1 ] ;then
 	echo "Formatting the NameNode"
-	./hadoop namenode -format
+	ssh $NAMENODE "$HADOOP_DIR/bin/hadoop namenode -format"
+
 fi
 
 if [ $MAP_REDUCE_ONLY -ne 1 ] ;then
-    ./start-dfs.sh
+
+	ssh $NAMENODE "$HADOOP_DIR/bin/hadoop-daemon.sh start namenode"
+
+    for dn in $DATANODE; do
+		ssh $dn "$HADOOP_DIR/bin/hadoop-daemon.sh start datanode"
+    done
 fi
 
 if [ $DFS_ONLY -ne 1 ] ; then
-./start-mapred.sh
+	ssh $JOBTRACKER "$HADOOP_DIR/bin/hadoop-daemon.sh start jobtracker"
+
+    for tt in $TASKTRACKER; do
+		ssh $tt "$HADOOP_DIR/bin/hadoop-daemon.sh start tasktracker"
+    done
 fi
 
-verifyJPS $SLAVES
+verifyJPS $NAMENODE
+verifyJPS $DATANODE
+verifyJPS $JOBTRACKER
+verifyJPS $TASKTRACKER
 
 echo "done"
